3aec8581e8c94ab8a378b40bf43ed5be7fc42f4e
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-26 08:43:02 -0600

    app.jsx refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,69 +2,126 @@ import Button from './Button/Button';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import SearchBar from './Searchbar/Searchbar';
-import { Component } from 'react';
+import { Component, useState, useEffect } from 'react';
 import { searchPhoto } from 'services/api';
 
-export default class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    isLoading: false,
-    totalImages: 0,
-  };
+// export default class App extends Component {
+//   state = {
+//     query: '',
+//     page: 1,
+//     images: [],
+//     isLoading: false,
+//     totalImages: 0,
+//   };
 
-  componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
+//   componentDidUpdate(_, prevState) {
+//     const { query, page } = this.state;
 
-    if (prevState.query !== query || prevState.page !== page) {
-      this.setState({ isLoading: true });
-      searchPhoto(query, page)
-        .then(data => {
-          const images = data.hits.map(
-            ({ id, tags, webformatURL, largeImageURL }) => {
-              return { id, tags, webformatURL, largeImageURL };
-            }
-          );
-          this.setState(prevState => {
-            return {
-              images: [...prevState.images, ...images],
-              totalImages: data.totalHits,
-            };
-          });
-        })
-        .catch(error => {
-          console.log(error);
-        })
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
-    }
-  }
+//     if (prevState.query !== query || prevState.page !== page) {
+//       this.setState({ isLoading: true });
+//       searchPhoto(query, page)
+//         .then(data => {
+//           const images = data.hits.map(
+//             ({ id, tags, webformatURL, largeImageURL }) => {
+//               return { id, tags, webformatURL, largeImageURL };
+//             }
+//           );
+//           this.setState(prevState => {
+//             return {
+//               images: [...prevState.images, ...images],
+//               totalImages: data.totalHits,
+//             };
+//           });
+//         })
+//         .catch(error => {
+//           console.log(error);
+//         })
+//         .finally(() => {
+//           this.setState({ isLoading: false });
+//         });
+//     }
+//   }
 
-  getQuery = query => {
-    if (query === this.state.query) {
+//   getQuery = query => {
+//     if (query === this.state.query) {
+//       return;
+//     }
+//     this.setState({ query, page: 1, images: [] });
+//   };
+//   handleLoadMore = () => {
+//     this.setState(prevState => {
+//       return { page: prevState.page + 1 };
+//     });
+//   };
+//   render() {
+//     const { isLoading, images, totalImages } = this.state;
+//     return (
+//       <>
+//         <SearchBar getQuery={this.getQuery} />
+//         <ImageGallery images={images} />
+
+//         {!isLoading && images.length < totalImages && (
+//           <Button handleLoadMore={this.handleLoadMore} />
+//         )}
+//         {isLoading && <Loader />}
+//       </>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalImages, setTotalImages] = useState(0);
+
+  useEffect(() => {
+    if (!query) {
       return;
     }
-    this.setState({ query, page: 1, images: [] });
-  };
-  handleLoadMore = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
-  };
-  render() {
-    const { isLoading, images, totalImages } = this.state;
-    return (
-      <>
-        <SearchBar getQuery={this.getQuery} />
-        <ImageGallery images={images} />
+    setIsLoading(true);
+    searchPhoto(query, page)
+      .then(data => {
+        const images = data.hits.map(
+          ({ id, tags, webformatURL, largeImageURL }) => {
+            return { id, tags, webformatURL, largeImageURL };
+          }
+        );
+        setImages(prevImages => [...prevImages, ...images]);
+        setTotalImages(data.totalHits);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [query, page]);
 
-        {!isLoading && images.length < totalImages && (
-          <Button handleLoadMore={this.handleLoadMore} />
-        )}
-        {isLoading && <Loader />}
-      </>
-    );
-  }
-}
+  const getQuery = newQuery => {
+    if (newQuery === query) {
+      return;
+    }
+    setQuery(newQuery);
+    setPage(1);
+    setImages([]);
+  };
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+  return (
+    <>
+      <SearchBar getQuery={getQuery} />
+      <ImageGallery images={images} />
+
+      {!isLoading && images.length < totalImages && (
+        <Button handleLoadMore={handleLoadMore} />
+      )}
+      {isLoading && <Loader />}
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 
+import { Example, Example2 } from './components/Example/Example.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+    {/* <Example />
+    <Example2 /> */}
+  </>
+  // </React.StrictMode>
 );
