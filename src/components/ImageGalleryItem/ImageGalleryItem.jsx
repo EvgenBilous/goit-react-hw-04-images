a95b57050b541235bad48086aea2b8f4126a3a50
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-26 09:20:42 -0600

    removed comments

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,39 +1,3 @@
-// import Modal from 'components/Modal/Modal';
-// import { useState } from 'react';
-
-// // export default class ImageGalleryItem extends Component {
-// //   state = {
-// //     showModal: false,
-// //   };
-
-// //   toggleModal = () => {
-// //     this.setState(prevState => {
-// //       return { showModal: !prevState.showModal };
-// //     });
-// //   };
-
-// //   render() {
-// //     const { image } = this.props;
-// //     return (
-// //       <li className="ImageGalleryItem">
-// //         <img
-// //           onClick={this.toggleModal}
-// //           className="ImageGalleryItem-image"
-// //           src={image.webformatURL}
-// //           alt={image.tags}
-// //         />
-// //         {this.state.showModal && (
-// //           <Modal
-// //             src={image.largeImageURL}
-// //             alt={image.tags}
-// //             closeModal={this.toggleModal}
-// //           />
-// //         )}
-// //       </li>
-// //     );
-// //   }
-// // }
-
 import React, { useState } from 'react';
 import Modal from 'components/Modal/Modal';
 
