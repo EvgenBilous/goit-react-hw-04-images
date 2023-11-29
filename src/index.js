7519edf7c20db863881da9df015627d71c83fc8b
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-29 10:16:45 -0600

    removed Example

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 
-import { Example, Example2 } from './components/Example/Example.jsx';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <>
     <App />
-    {/* <Example />
-    <Example2 /> */}
   </>
   // </React.StrictMode>
 );
